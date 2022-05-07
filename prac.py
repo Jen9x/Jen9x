9db3069ea952d98c2d8ab63cbe9778efdb62a8ee
Jigree don <Jenish kathayat>
2022-05-06 19:43:04 -0500

    practice to push

---
+++ prac.py
@@ -0,0 +1,6 @@
+from cmath import cos
+import math
+side1 = input("Enter the first side: ")
+side2 = input("Enter the Second side: ")
+angle = input("Enter the angle between them(Degree): ")
+a = cos 45
